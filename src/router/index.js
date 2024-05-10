6d9b5328d8207ca55d517aaf9918b3d1162661c7
VitalikL <lim.vitaliy@gmail.com>
2024-05-10 10:22:35 -0400

    Adding BASE_URL

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import DockSearch from '../components/Dock/DockSearch.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/obs-sda-hymnal/'),
   routes: [
     {
       path: '/',
