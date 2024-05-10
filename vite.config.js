c0b68569e42c4852221c4f09e84d963d5b0fb475
VitalikL <lim.vitaliy@gmail.com>
2024-05-10 10:28:27 -0400

    Adding BASE_URL

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/obs-sda-hymnal/',
+  publicPath: '/obs-sda-hymnal/',
   plugins: [
     vue(),
   ],
